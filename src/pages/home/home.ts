4ac555b866c8231acd3c87b73fe36bac1c041395
Jony Developer <jonatan@JONAS-PUNK.local>
2018-04-30 13:16:31 -0500

    Segundo

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { AlertController, NavController } from 'ionic-angular';
 import { Camera, CameraOptions } from '@ionic-native/camera';
+import { Geolocation } from '@ionic-native/geolocation';
 
 @Component({
   selector: 'page-home',
@@ -8,36 +9,82 @@ import { Camera, CameraOptions } from '@ionic-native/camera';
 })
 export class HomePage {
 
-    constructor(public navCtrl: NavController, public _camera: Camera) { }
+    position = {
+        lat: 0,
+        lon: 0
+    };
 
-    public tomarFoto(){
-        const options: CameraOptions = {
-            quality: 100,
-            destinationType: this._camera.DestinationType.DATA_URL,
-            encodingType: this._camera.EncodingType.JPEG,
-            mediaType: this._camera.MediaType.PICTURE
-        }
-        
-        this._camera.getPicture(options).then((imageData) => {
-            
-        }, (err) => {
-            console.log(err);
+    constructor(public navCtrl: NavController, public _camera: Camera, private geolocation: Geolocation, public _alertController:AlertController) { }
+    
+    public geolocalizacion(){
+        this.geolocation.getCurrentPosition().then((resp) => {
+            this.position.lat = resp.coords.latitude;
+            this.position.lon = resp.coords.longitude;
+            console.log(this.position);
+            return true;
+        }).catch((error) => {
+             console.log('Error getting location', error);
+             return false;
         });
     }
 
-    public tomarVideo(){
-        const options: CameraOptions = {
-            quality: 100,
-            destinationType: this._camera.DestinationType.DATA_URL,
-            encodingType: this._camera.EncodingType.JPEG,
-            mediaType: this._camera.MediaType.VIDEO
-        }
-        
-        this._camera.getPicture(options).then((videoData) => {
+    public tomarFoto(){
+        if(this.geolocalizacion){
+            const options: CameraOptions = {
+                quality: 100,
+                destinationType: this._camera.DestinationType.DATA_URL,
+                encodingType: this._camera.EncodingType.JPEG,
+                mediaType: this._camera.MediaType.PICTURE
+            }
+            this._camera.getPicture(options).then((imageData) => {
             
-        }, (err) => {
-            console.log(err);
-        });
+            }, (err) => {
+                console.log(err);
+            });
+        } else {
+            let confirm = this._alertController.create({
+                title: 'Ubicación',
+                message: 'No he podido identificar tu ubicación, por favor verifica que esté prendida',
+                buttons: [
+                  {
+                    text: 'Ok',
+                    handler: () => {
+                      console.log('Disagree clicked');
+                    }
+                  }
+                ]
+                });
+            confirm.present();
+        }
+    }
+
+    public tomarVideo(){
+        if(this.geolocalizacion){
+            const options: CameraOptions = {
+                quality: 100,
+                destinationType: this._camera.DestinationType.DATA_URL,
+                mediaType: this._camera.MediaType.VIDEO
+            }
+            this._camera.getPicture(options).then((imageData) => {
+            
+            }, (err) => {
+                console.log(err);
+            });
+        } else {
+            let confirm = this._alertController.create({
+                title: 'Ubicación',
+                message: 'No he podido identificar tu ubicación, por favor verifica que esté prendida',
+                buttons: [
+                  {
+                    text: 'Ok',
+                    handler: () => {
+                      console.log('Disagree clicked');
+                    }
+                  }
+                ]
+                });
+            confirm.present();
+        }
     }
 
 }
