0b5c96791906420651e000aeb6dd6469d8427ad8
Jony Developer <jonatan@JONAS-PUNK.local>
2018-04-30 12:47:08 -0500

    Primer

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Camera, CameraOptions } from '@ionic-native/camera';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,36 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+    constructor(public navCtrl: NavController, public _camera: Camera) { }
 
-  }
+    public tomarFoto(){
+        const options: CameraOptions = {
+            quality: 100,
+            destinationType: this._camera.DestinationType.DATA_URL,
+            encodingType: this._camera.EncodingType.JPEG,
+            mediaType: this._camera.MediaType.PICTURE
+        }
+        
+        this._camera.getPicture(options).then((imageData) => {
+            
+        }, (err) => {
+            console.log(err);
+        });
+    }
+
+    public tomarVideo(){
+        const options: CameraOptions = {
+            quality: 100,
+            destinationType: this._camera.DestinationType.DATA_URL,
+            encodingType: this._camera.EncodingType.JPEG,
+            mediaType: this._camera.MediaType.VIDEO
+        }
+        
+        this._camera.getPicture(options).then((videoData) => {
+            
+        }, (err) => {
+            console.log(err);
+        });
+    }
 
 }
