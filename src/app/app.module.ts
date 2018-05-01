6636ea81da483475fd017b528a89e67ff1aae71d
Jony Developer <jonatan@JONAS-PUNK.local>
2018-05-01 16:18:44 -0500

    Video

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { Camera } from '@ionic-native/camera';
+import { MediaCapture } from '@ionic-native/media-capture';
 import { Geolocation } from '@ionic-native/geolocation';
 import { ScreenOrientation } from '@ionic-native/screen-orientation';
 
@@ -29,6 +30,7 @@ import { HomePage } from '../pages/home/home';
     Camera,
     Geolocation,
     ScreenOrientation,
+    MediaCapture,
     SplashScreen,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
